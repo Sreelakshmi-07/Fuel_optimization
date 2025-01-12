c5d2cceb330994f2ff0a45106fa58a4889b6f1a6
sreelakshmi-07 <sreelakshmi0777@gmail.com>
2025-01-12 14:14:39 +0400

    urls added

--- fuel_optimization/geolocation/urls.py
+++ fuel_optimization/geolocation/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('fuel-route/', views.fuel_route_api, name='fuel-route'),
+    path('fuel-route/', views.fetch_route_from_ors, name='fuel-route'),
+    path('map/', views.map_view, name='map'),
 ]
